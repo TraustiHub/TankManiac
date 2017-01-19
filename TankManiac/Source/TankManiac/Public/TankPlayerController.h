b92be774e3b34830eb5b05d01f14e45c5e399907
Stefan Trausti Arnason <stefan@trausti.no>
2017-01-19 08:42:23 +0100

    Tick -> PlayerController aim towards crosshair

--- TankManiac/Source/TankManiac/Public/TankPlayerController.h
+++ TankManiac/Source/TankManiac/Public/TankPlayerController.h
@@ -18,4 +18,11 @@ public:
 	ATank* GetControlledTank() const;
 
 	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+
+	// Start the tank moving the barrel so that a shot would hit where
+	// the crosshair intersects the world
+	void AimTowardsCrosshair();
 };
