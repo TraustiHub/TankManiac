47e0a9095ca4d353a4b64cda40e0362f7a7d3f3b
Stefan Trausti Arnason <stefan@trausti.no>
2017-01-19 13:46:34 +0100

    HitLocation ( sight or crosshair )

--- TankManiac/Source/TankManiac/Private/TankPlayerController.cpp
+++ TankManiac/Source/TankManiac/Private/TankPlayerController.cpp
@@ -35,7 +35,18 @@ void ATankPlayerController::AimTowardsCrosshair()
 {
 	if (!GetControlledTank()) { return; }
 
-	// Get world location if linetrace through crosshair
-	// If it hits the landscape
-		// Tell controlled tank to aim at this point
+	FVector HitLocation; // Out parameter
+	if (GetSightRayHitLocation(HitLocation)) // Has "side-effect", is going to line trace
+	{
+		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+		
+		// TODO Tell controlled tank to aim at this point
+	}
+}
+
+// Get world location of linetrace through crosshair, true if hits landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
+{
+	HitLocation = FVector(1.0);
+	return true;
 }
--- TankManiac/Source/TankManiac/Public/TankPlayerController.h
+++ TankManiac/Source/TankManiac/Public/TankPlayerController.h
@@ -25,4 +25,7 @@ public:
 	// Start the tank moving the barrel so that a shot would hit where
 	// the crosshair intersects the world
 	void AimTowardsCrosshair();
+
+	// Return an OUT parameter, true if hit landscape
+	bool GetSightRayHitLocation(FVector& HitLoaction) const;
 };
