42132180f93f9565f74206e1e6dfff23e7e8ea86
Stefan Trausti Arnason <stefan@trausti.no>
2017-01-20 11:10:01 +0100

    Return x,y,z hit location of the crosshair in the landscape

--- TankManiac/Source/TankManiac/Public/TankPlayerController.h
+++ TankManiac/Source/TankManiac/Public/TankPlayerController.h
@@ -35,5 +35,9 @@ private:
 	UPROPERTY(EditAnywhere)
 	float CrosshairYLocation = 0.3333;
 
+	UPROPERTY(EditAnywhere)
+	float LineTraceRange = 1000000;
+
 	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
 };
