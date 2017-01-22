c23105ba072381ca98de77cbac55e6417ed9aff2
Stefan Trausti Arnason <stefan@trausti.no>
2017-01-22 17:30:15 +0100

    Create Default Sub Object in C++ for the aiming component - Whatever that means.

--- TankManiac/Source/TankManiac/Public/Tank.h
+++ TankManiac/Source/TankManiac/Public/Tank.h
@@ -2,8 +2,9 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "GameFramework/Pawn.h"
-#include "Tank.generated.h"
+#include "Tank.generated.h" // Put new includes above
 
 UCLASS()
 class TANKMANIAC_API ATank : public APawn
@@ -12,6 +13,9 @@ class TANKMANIAC_API ATank : public APawn
 
 public:
 	void AimAt(FVector HitLocation);
+
+protected:
+	UTankAimingComponent* TankAimingComponent = nullptr;
 
 private:
 	// Sets default values for this pawn's properties
