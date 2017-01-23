119cc5ec1b7b7be02a4dc814e83135df283c1b05
Stefan Trausti Arnason <stefan@trausti.no>
2017-01-23 14:39:57 +0100

    Creating TankBarrel.cpp and using forward declarations

--- TankManiac/Source/TankManiac/Public/Tank.h
+++ TankManiac/Source/TankManiac/Public/Tank.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h" // Put new includes above
 
+class UTankBarrel;
+
 UCLASS()
 class TANKMANIAC_API ATank : public APawn
 {
@@ -15,7 +17,7 @@ public:
 	void AimAt(FVector HitLocation);
 
 	UFUNCTION(BlueprintCallable, Category = Setup)
-	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+	void SetBarrelReference(UTankBarrel* BarrelToSet);
 
 protected:
 	UTankAimingComponent* TankAimingComponent = nullptr;
