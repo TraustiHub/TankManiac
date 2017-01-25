0fb3b2e6d9abd90a52af170db663dc1b2e6d7e7a
Stefan Trausti Arnason <stefan@trausti.no>
2017-01-25 08:47:47 +0100

    Projectile fires

--- TankManiac/Source/TankManiac/Public/Projectile.h
+++ TankManiac/Source/TankManiac/Public/Projectile.h
@@ -20,6 +20,9 @@ public:
 	// Called every frame
 	virtual void Tick( float DeltaSeconds ) override;
 
-	
+	void LaunchProjectile(float Speed);
+
+private:
+	UProjectileMovementComponent* ProjectileMovement = nullptr;
 	
 };
