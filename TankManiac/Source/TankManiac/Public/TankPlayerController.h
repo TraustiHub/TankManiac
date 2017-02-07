de687fcc95dd62d8f95d090793c70719aac30645
Stefan Trausti Arnason <stefan@trausti.no>
2017-02-07 17:56:06 +0100

    Tank aiming - restructure to make the chrosshair color effect working again.

--- TankManiac/Source/TankManiac/Public/TankPlayerController.h
+++ TankManiac/Source/TankManiac/Public/TankPlayerController.h
@@ -6,9 +6,10 @@
 #include "TankPlayerController.generated.h" // Must be the last include
 
 class ATank;
+class UTankAimingComponent;
 
 /**
- * 
+ * Responible for helping the player aim.
  */
 UCLASS()
 class TANKMANIAC_API ATankPlayerController : public APlayerController
@@ -18,6 +19,9 @@ class TANKMANIAC_API ATankPlayerController : public APlayerController
 protected:
 	UFUNCTION(BlueprintCallable, Category = "Setup")
 	ATank* GetControlledTank() const;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Setup")
+		void FoundAimingComponent(UTankAimingComponent* AimCompRef);
 
 private:
 	virtual void BeginPlay() override;
