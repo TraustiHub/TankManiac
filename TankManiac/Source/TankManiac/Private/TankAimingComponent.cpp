1b42286f7f19dbdd8342dd11073ff27320044cbd
Stefan Trausti Arnason <stefan@trausti.no>
2017-02-08 09:04:57 +0100

    inserting "ensure" to discover why barrel is not rotating towards the aiming location

--- TankManiac/Source/TankManiac/Private/TankAimingComponent.cpp
+++ TankManiac/Source/TankManiac/Private/TankAimingComponent.cpp
@@ -24,7 +24,7 @@ void UTankAimingComponent::Initialise(UTankBarrel* BarrelToSet, UTankTurret* Tur
 
 void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 {
-	if (!Barrel) { return; }
+	if (!ensure(Barrel)) { return; }
 
 	FVector OutLaunchVelocity;
 	FVector StartLocation = Barrel->GetSocketLocation(FName("Projectile"));
@@ -50,7 +50,7 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 
 void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 {
-	if (!Barrel || !Turret) { return; }
+	if (!ensure(Barrel || !Turret)) { return; }
 
 	// Work-out difference between current barrel rotaition, and AimDirection
 	auto BarrelRotator = Barrel->GetForwardVector().Rotation();
