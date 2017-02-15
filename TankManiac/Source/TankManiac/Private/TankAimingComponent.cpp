0aa7e5edd6ac8f253c1c0efad0ccdf5957cfc4a7
Stefan Trausti Arnason <stefan@trausti.no>
2017-02-15 15:00:15 +0100

    Improve aiming - Always take the shortest roote

--- TankManiac/Source/TankManiac/Private/TankAimingComponent.cpp
+++ TankManiac/Source/TankManiac/Private/TankAimingComponent.cpp
@@ -85,8 +85,16 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 	auto AimAsRotator = AimDirection.Rotation();
 	auto DeltaRotator = AimAsRotator - BarrelRotator;
 	
+	// Always yaw the shortest way
 	Barrel->Elevate(DeltaRotator.Pitch); 
-	Turret->Rotate(DeltaRotator.Yaw);
+	if (FMath::Abs(DeltaRotator.Yaw) < 180)
+	{
+		Turret->Rotate(DeltaRotator.Yaw);
+	}
+	else
+	{
+		Turret->Rotate(-DeltaRotator.Yaw);
+	}
 }
 
 void UTankAimingComponent::Fire()
