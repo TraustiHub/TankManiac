f071218bdb17664b3126d5e9f21db8b5dd0254f7
Stefan Trausti Arnason <stefan@trausti.no>
2017-02-15 17:39:49 +0100

    Making AI tanks stop 80m away from us - find Setup in TankAIController_BP

--- TankManiac/Source/TankManiac/Private/TankAimingComponent.cpp
+++ TankManiac/Source/TankManiac/Private/TankAimingComponent.cpp
@@ -43,6 +43,11 @@ void UTankAimingComponent::TickComponent(float DeltaTime, enum ELevelTick TickTy
 	}
 }
 
+EFiringState UTankAimingComponent::GetFiringState() const
+{
+	return FiringState;
+}
+
 bool UTankAimingComponent::IsBarrelMoving()
 {
 	if (!ensure(Barrel)) { return false; }
--- TankManiac/Source/TankManiac/Public/TankAIController.h
+++ TankManiac/Source/TankManiac/Public/TankAIController.h
@@ -13,12 +13,14 @@ class TANKMANIAC_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 	
+protected:
+	// At what radius will the AI stop moving towards the player (in cm?)
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	float AcceptanceRadius = 8000;
+
 private:
 	virtual void BeginPlay() override;
 
 	virtual void Tick(float DeltaSeconds) override;
-
-	// At what radius will the AI stop moving towards the player (in cm?)
-	float AcceptanceRadius = 3000;
 
 };
