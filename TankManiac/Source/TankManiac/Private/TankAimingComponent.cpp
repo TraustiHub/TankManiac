5d9c856c4c0b6a4dae832a75f019f20ff0bd7434
Stefan Trausti Arnason <stefan@trausti.no>
2017-02-23 15:13:14 +0100

    Preparing for Particles - Changing the fire to 20 rounds and 0.1sec reload time

--- TankManiac/Source/TankManiac/Private/TankAimingComponent.cpp
+++ TankManiac/Source/TankManiac/Private/TankAimingComponent.cpp
@@ -47,7 +47,7 @@ void UTankAimingComponent::TickComponent(float DeltaTime, enum ELevelTick TickTy
 	}
 }
 
-int UTankAimingComponent::GetRoundsLeft() const
+int32 UTankAimingComponent::GetRoundsLeft() const
 {
 	return RoundsLeft;
 }
